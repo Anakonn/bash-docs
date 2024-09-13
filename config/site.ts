e1b7d91b90f3ec31f610ce0b192704b236bb4f12
ego <mrego@anakonn.com>
2024-09-13 18:45:50 +0900

    hi

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+	title: 'Bash Docs',
+	url: 'https://bash.docsforall.com/',
+	googleAdsSrc:
+		'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2130210715518535',
+	githubUrl: 'https://github.com/Anakonn/bash-docs',
+};
